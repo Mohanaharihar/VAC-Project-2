f2d039850346bed441962c5fa4fdcf217c7d0f74
Mohanaharihar <114056114+Mohanaharihar@users.noreply.github.com>
2023-04-12 10:54:49 +0530

    Create RollADie.py

---
+++ RollADie.py
@@ -0,0 +1,10 @@
+import random
+min = 1
+max = 6
+
+print ("Rolling the dices...")
+print ("The numbers are....")
+print (random.randint(min, max))
+print (random.randint(min, max))
+
+roll = print(":)")
